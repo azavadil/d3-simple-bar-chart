13594c32924dd55a1f086eb1fe6c7850d30a2bb1
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-07-07 21:44:32 -0700

    d3 notes

--- d3-tmp/dthree.js
+++ d3-tmp/dthree.js
@@ -47,7 +47,50 @@ for(var i = 0; i < 10; i++){
   data1.cash.push({'count':Math.random()*10000});
 }
 
+var params = {};
+params.width = 700;
+params.height = 300;
+
+function draw2(data){
+  d3.select("body")
+    .append("svg")
+      .attr("width", param.width)
+      .attr("height", params.height)
+    .selectAll("circle") // empty selection
+    .data(data)  // join with data
+    .enter()  // get enter selection
+    append("circle");  // append as circle
+}
+
+// finding min and max values
+// d3.extent is how you find the min and max values of data
+// second argument selects the attribute to calculate min and max on
+// usage
+// var x_scale = d3.scale.linear()
+//   .range([margin, width-margin])
+//   .domain(x_extent)
+// var x_extent = d3.extent(data, function(d){ return d.collision_with_injury; })
+
+
+// creating axes. pass the scale constructor the scale object
+// var x_axis = d3.svg.axis().scale(x_scale);
 
 draw0(data0);
 draw1(data1);
 
+
+/*
+  d3.select("svg")
+    .append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + (height-margin) + ")")
+    .call(x_axis)
+
+  1. use transform to move the axis group down to the bottom of the graph
+     SVG transforms take an existing element and either rotates them or
+     moves them around. The translate transform just moves elements around
+     Here the x_axis group is moved 0 pixels to the right and height-margin
+     pixels down from the top.
+
+  2. using .call() to actually draw the axis. All this does is call the
+     the time_axis function, passing in the current selection.
