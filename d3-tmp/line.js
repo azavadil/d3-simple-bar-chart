ede714bebd5149c804b7b7ef752258fb6b2612cc
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-07-08 10:52:11 -0700

    simple line graph

--- d3-tmp/line.js
+++ d3-tmp/line.js
@@ -98,6 +98,53 @@ var draw0 = function(data){
     .attr("transform", "translate(0," + height + ")")
     .call(time_axis);
 
+  var count_axis = d3.svg.axis()
+    .scale(count_scale)
+    .orient("left");
+
+  d3.select("svg")
+    .append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(" + margin + ",0)")
+    .call(count_axis);
+
+  /** path generator
+   * @param{ accessor function } - function to return x value
+   * @param{ accessor function } - function to return y value
+     d3.svg.line() generates a function that takes in a data set and 
+     outputs and SVG path element
+
+     we pass the lineGeneratorFn a data set and it returns a line path 
+
+  */
+  var lineGeneratorFn = d3.svg.line()
+    .x(function(d){ return time_scale(d.time); })
+    .y(function(d){ return count_scale(d.count); });
+
+  d3.select("svg")
+    .append("path")
+      .attr("d", lineGeneratorFn(data.times_square))
+      .attr("class", "times_square")
+
+  d3.select("svg")
+    .append("path")
+      .attr("d", lineGeneratorFn(data.grand_central))
+      .attr("class", "grand_central");
+
+  d3.select(".y.axis")
+    .append("text")
+    .text("mean number of turnstile revolutions")
+    .attr("transform", "rotate (90, " + -margin + ", 0)")
+    .attr("x", 20)
+    .attr("y", 0);
+
+  d3.select(".x.axis")
+    .append("text")
+      .text("time")
+      .attr("x", function(){return (width/1.6) - margin; })
+      .attr("y", margin/1.5);
+
+
 
 }
 
